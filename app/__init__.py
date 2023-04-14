ccd85d96303a97c275944e587ab128eaaff76085
DinoG73 <53279025+DinoG73@users.noreply.github.com>
2023-04-14 16:15:43 -0300

    Add MYSQL connection

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,16 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 mysql = MySQL()
 
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'empleados'
+
 if __name__ == '__main__':
     app.run(debug=True)
 
 
-from app import routes
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# from app import routes
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
