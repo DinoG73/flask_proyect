c09e42af615902d6b8be3d698701036b53a1c29a
DinoG73 <53279025+DinoG73@users.noreply.github.com>
2023-04-19 13:05:51 -0300

    Problems showing db items

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 from flaskext.mysql import MySQL
 
 
@@ -14,23 +14,45 @@ app.config['MYSQL_DATABASE_DB'] = 'empleados'
 mysql.init_app(app)
 
 #from app import routes
-@app.route("/")
+@app.route('/')
 # def hello_world():
 #      return "<p>Hello, World!</p>"
 
 def index():
-    # conn = mysql.connect()
-    # cursor = conn.cursor()
+    conn = mysql.connect()
+    cursor = conn.cursor()
 
-    # sql = "insert into empleados (nombre, correo, foto) values ('Bruno', 'bag@gmail.com', 'fotobruno.jpg');"
-    # cursor.execute(sql)
-    # conn.commit()
+    sql = "SELECT * FROM empleados;"
+    cursor.execute(sql)
+
+    empleado = cursor.fetchall
+
+    print(empleado)
+
+    conn.commit()
+
+    return render_template('empleados/index.html', empleados=empleado)
+
+@app.route('/create')
+def create():
+    return render_template('empleados/create.html')
+
+
+@app.route('/store', methods=["POST"])
+def store():
+    _nombre = request.form['txt_name']
+    _correo = request.form['txt_email']
+    _foto = request.files['txt_picture']
+
+    sql = "INSERT INTO empleados (nombre, correo, foto) values (%s,%s, %s);"
+    datos = (_nombre, _correo, _foto.filename)
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql, datos)
+    conn.commit()
 
     return render_template('empleados/index.html')
-
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
